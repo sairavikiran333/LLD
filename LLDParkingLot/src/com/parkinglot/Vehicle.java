aa136165147eb0841d6478923f5adfd14c097d0e
sappalab <sappalab@SAPPALAB-7420.oradev.oraclecorp.com>
2023-12-17 15:16:42 +0530

    ParkingLot

---
+++ LLDParkingLot/src/com/parkinglot/Vehicle.java
@@ -0,0 +1,22 @@
+package com.parkinglot;
+
+//Vehicle.java
+
+
+public class Vehicle {
+ protected String licensePlate;
+ protected VehicleType type;
+
+ public Vehicle(String licensePlate, VehicleType type) {
+     this.licensePlate = licensePlate;
+     this.type = type;
+ }
+
+ public String getLicensePlate() {
+     return licensePlate;
+ }
+
+ public VehicleType getType() {
+     return type;
+ }
+}
---
+++ LLDParkingLot/src/com/parkinglot/VehicleType.java
@@ -0,0 +1,5 @@
+package com.parkinglot;
+
+public enum VehicleType {
+	 MOTORCYCLE, CAR, BUS
+}
